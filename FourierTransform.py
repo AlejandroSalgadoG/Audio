691fd4ccdb8ec980922d7b470fbb3b82d2ab20e2
Alejandro Salgado G <asalgad2@eafit.edu.co>
2021-04-06 11:39:43 -0500

    Improving code to plot fourier transform

--- FourierTransform.py
+++ FourierTransform.py
@@ -2,13 +2,13 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
-from Auxiliar import read_data
+from Auxiliar import read_data, apply_fourier, get_magnitude_phase
 
-signal = read_data( "Records/a.wav" )
+signal = read_data( "Records/c1.wav" )
 signal_idx = np.arange( signal.size ) 
 
 fs = 44032
-cut = 80000
+cut = 44032
 x = signal[cut:cut+fs]
 x_idx = signal_idx[cut:cut+fs]
 
@@ -16,22 +16,8 @@ plt.plot(signal_idx, signal)
 plt.plot(x_idx, x)
 plt.show()
 
-n = x.size
-dt = 1/n # inter sample time
-p = n*dt # period
-df = 1/p # frequency resolution
-#print(df)
+n, dt, p, df, nyquist, t, coef = apply_fourier( x )
+amps, phas = get_magnitude_phase( coef[:nyquist] / n )
 
-t = np.arange(0,p,dt) # time
-nyquist = math.floor( t.size/2 ) + 1
-coef = np.fft.fft( x )[:nyquist] / n
-
-fig, (ax1, ax2) = plt.subplots(2)
-
-ax1.scatter( np.arange(coef.size), abs(coef)*2, s=7 )
-ax2.scatter( np.arange(coef.size), np.angle(coef), s=7 )
-
-ax1.set_ylabel("Amplitude")
-ax2.set_ylabel("Phase")
-ax2.set_xlabel("Frequency")
+plt.plot( np.arange(nyquist), amps )
 plt.show()
