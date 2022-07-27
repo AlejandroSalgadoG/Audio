99db99af7074cb8fda01333824a91ce6827d5bff
Alejandro Salgado <alejandro.salgado@globant.com>
2022-07-27 09:03:24 -0500

    Improve animation

--- Animation.py
+++ Animation.py
@@ -12,29 +12,65 @@ def norm(data):
     return (data - min_s) / (max_s - min_s)
 
 reader = WavReader(f"Data/vowels/all/all.wav")
+data = reader.get_data()
+
+split1 = batch_data(data, size=max_freq*2)
+split2 = batch_data(data, size=max_freq*2, offset=max_freq)
 
 plt.ion()
-fig, (ax1, ax2) = plt.subplots(2)
 
-for data, start, end in batch_data(reader.get_data(), size=max_freq*2):
-    freq_data = FrequencyDomain(data)
-    amps = freq_data.get_amplitudes()[:max_freq]
-    norm_amps = norm(amps)
+fig, axis = plt.subplot_mosaic([
+    ["signal",   "signal"  ],
+    ["sub1",     "sub2"    ],
+    ["fourier1", "fourier2"],
+])
 
-    ax1.plot(data, linewidth=1, color="red")
-    ax2.plot(norm_amps, linewidth=1, color="red")
+axis["signal"].plot(data, linewidth=1, color="blue")
+axis["signal"].set_xlim([0, reader.n_data_samples])
+axis["signal"].set_ylim([-6e3, 6e3])
 
-    ax1.set_xlim([0, max_freq * 2])
-    ax1.set_ylim([-5e3, 5e3])
+for (batch1, start1, end1), (batch2, start2, end2) in zip(split1, split2):
+    freq_data1 = FrequencyDomain(batch1)
+    amps1 = freq_data1.get_amplitudes()[:max_freq]
+    norm_amps1 = norm(amps1)
 
-    ax2.set_xlim([0, max_freq])
-    ax2.set_ylim([0, 1])
+    freq_data2 = FrequencyDomain(batch2)
+    amps2 = freq_data2.get_amplitudes()[:max_freq]
+    norm_amps2 = norm(amps2)
+
+    axis["signal"].plot([start1, end1], [5e3, 5e3], color="red", linewidth=1)
+    axis["signal"].plot([start2, end2], [4.5e3, 4.5e3], color="green", linewidth=1)
+
+    axis["sub1"].plot(batch1, linewidth=1, color="red")
+    axis["fourier1"].plot(norm_amps1, linewidth=1, color="red")
+
+    axis["sub2"].plot(batch2, linewidth=1, color="green")
+    axis["fourier2"].plot(norm_amps2, linewidth=1, color="green")
+
+    axis["sub1"].set_xlim([0, max_freq * 2])
+    axis["sub1"].set_ylim([-5e3, 5e3])
+
+    axis["sub2"].set_xlim([0, max_freq * 2])
+    axis["sub2"].set_ylim([-5e3, 5e3])
+
+    axis["fourier1"].set_xlim([0, max_freq])
+    axis["fourier1"].set_ylim([0, 1])
+
+    axis["fourier2"].set_xlim([0, max_freq])
+    axis["fourier2"].set_ylim([0, 1])
 
     plt.draw()
-    plt.pause(1)
+    #plt.pause(1)
+    plt.waitforbuttonpress()
 
-    ax1.cla()
-    ax2.cla()
+    axis["signal"].plot([start1, end1], [5e3, 5e3], color="white", linewidth=1)
+    axis["signal"].plot([start2, end2], [4.5e3, 4.5e3], color="white", linewidth=1)
+
+    axis["sub1"].cla()
+    axis["fourier1"].cla()
+
+    axis["sub2"].cla()
+    axis["fourier2"].cla()
 
 plt.show(block=True)
 
